98254b83ac965f98241695781a07a318be07217e
Nallelyht <nallelyht@gmail.com>
2017-04-22 13:37:30 -0500

    Update ejercicio-01.js

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,6 +1,9 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-        if(typeof creditCardNumber==="undefined"){return "Ingresa un numero";}
+        if(typeof creditCardNumber==="undefined"){
+            return "Ingresa un numero";
+        }
+    
         var creditNumber = creditCardNumber.toString();
         var string=creditNumber.split("");
         var numPares=[];
@@ -9,40 +12,47 @@ function isValidCard(creditCardNumber) {
         var numerosSumados=[];
         var num2=[];   
 
-        if(typeof creditCardNumber!="number"){return "Error de dato"; }
-        if(creditNumber.length!=16){return "Faltan numeros";} 
-        if(creditNumber===""){return "Ingresa un numero"}   
+        if(typeof creditCardNumber!="number"){
+            return "Error de dato"; 
+        }
+        if(creditNumber.length!=16){
+            return "Faltan numeros";
+        } 
+        if(creditNumber===""){
+            return "Ingresa un numero"
+        }   
             
-            string.map(function(num,i){
-                if(i%2===0){
-                return numPares.push(num);
-                }else {
-                return numImpares.push(parseInt(num));
-                }});
-                
-            numerosSumados = numPares.map(function(num){
-                var num1=num*2;
-                if(num1>9){
-                    return num1-9
-                }else{
-                    return num1
-                }
-                });
-                
-                
-            num2 = numerosSumados.map(function (num,i){
-                    return num + numImpares[i];
-                });
-                
-            numeroTotal = num2.reduce(function(valorAnterior, valorActual, indice, vector){
-                return valorAnterior + valorActual;
-                });
-                
-            if(numeroTotal%10===0){
-                return true;
+        string.map(function(num,i){
+            if(i%2===0){
+            return numPares.push(num);
+            }else {
+            return numImpares.push(parseInt(num));
+            }});
+
+        numerosSumados = numPares.map(function(num){
+            var num1=num*2;
+            if(num1>9){
+                return num1-9
             }else{
-                return false;
+                return num1
             }
+            });
+
+
+        num2 = numerosSumados.map(function (num,i){
+                return num + numImpares[i];
+            });
+
+        numeroTotal = num2.reduce(function(valorAnterior, valorActual, indice, vector){
+            return valorAnterior + valorActual;
+            });
+
+        if(numeroTotal%10===0){
+            return true;
+        }else{
+            return false;
+        }
+
     
     
 }
